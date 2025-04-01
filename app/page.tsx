5cac5b84dd0cf8cb64fe944a5a3582fd2dbc6f2d
Nick-Raal <105031104+Nick-Raal@users.noreply.github.com>
2025-04-01 15:25:20 -0700

    changed some cosmetic things

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,16 @@ import Head from 'next/head';
 import TextEntry from './entrybox';
 import React from 'react';
 
+export const metadata = {
+  title: "Differential Equations Solver",
+  description: "My description",
+}
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Main Page</title>
+        <title>Differential Equations Solver</title>
         <meta name="description" content="Main page heading here" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-between p-5 bg-gray-900 text-white">
